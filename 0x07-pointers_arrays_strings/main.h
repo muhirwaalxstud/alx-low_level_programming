90298f6ba15452c771dd79c8dbf5eb0f9c048a89
muhirwajeanbosco <muhirwajb@workmail.com>
2024-01-08 12:17:49 -0800

    main.h

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype for _memset function */
+char *_memset(char *s, char b, unsigned int n);
+
+/* Prototype for _putchar function */
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
