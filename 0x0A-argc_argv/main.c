53ffeb8a85c64fb3cccecf51fcfaa9a4753e18c9
muhirwajeanbosco <muhirwajb@workmail.com>
2024-01-11 23:50:50 -0800

    main.c

---
+++ 0x0A-argc_argv/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc; /* Suppress unused parameter warning */
+
+    printf("%s\n", argv[0]);
+
+    return (0);
+}
+
