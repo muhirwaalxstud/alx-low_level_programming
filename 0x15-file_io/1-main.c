5eb50c430618af732fcb38fec3538f3d62a5c8a0
muhirwalxstud <inklabel72@gmail.com>
2024-02-13 05:31:36 -0800

    Under the snow

---
+++ 0x15-file_io/1-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @ac: Number of arguments
+ * @av: Array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int ac, char **av)
+{
+    int res;
+
+    if (ac != 3)
+    {
+        fprintf(stderr, "Usage: %s filename text\n", av[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    res = create_file(av[1], av[2]);
+    printf("-> %i\n", res);
+
+    return (0);
+}
+
